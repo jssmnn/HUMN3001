529c1b1292fea015bec021332642974f234305e8
jssmnn <you@example.com>
2021-05-26 16:39:22 +1000

    changes to colour and js format

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let sky = document.getElementById("sky");
+let ground = document.getElementById("ground");
+let vineLarge = document.getElementById("vineFront");
+let text = document.getElementById("text");
+let clouds = document.getElementById("clouds");
+
+
+window.addEventListener('scroll', function () {
+    var value = window.scrollY;
+    sky.style.top = -value * 0.2 + 'px';
+    vineLarge.style.top = value * 1 + 'px';
+    ground.style.top = value * 0.02 + 'px';
+    clouds.style.left = value * -0.5 + 'px';
+
+})
