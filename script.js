801eb02ef691705c9528ea226db663f5f4f0a916
jssmnn <you@example.com>
2021-05-27 12:14:14 +1000

    added images

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let ground = document.getElementById("ground");
 let vineLarge = document.getElementById("vineFront");
 let text = document.getElementById("text");
 let clouds = document.getElementById("clouds");
+let moon2 = document.getElementById("moon2");
 
 
 window.addEventListener('scroll', function () {
@@ -12,5 +13,6 @@ window.addEventListener('scroll', function () {
     vineLarge.style.top = value * 1 + 'px';
     ground.style.top = value * 0.02 + 'px';
     clouds.style.left = value * -0.5 + 'px';
+    moon2.style.top = value * -0.5 + 'px';
 
 })
